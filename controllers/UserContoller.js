a638a713d0f91573fb68027a16ef94e32a0f4bb8
capimaker <capimaker@gmail.com>
2025-05-30 15:40:03 +0200

    se ha creado ruta users y UserController funcionales

---
+++ controllers/UserContoller.js
@@ -0,0 +1,13 @@
+const {User} = require('../models/index.js');
+
+const UserController = {
+    async create (req, res){
+        try{
+            const user = await User.create(req.body)
+            res.status(201).send({msg:'Usuario creado con éxito', user})
+        }catch(error){
+            res.status(500).send(error)
+        }
+    }
+}
+module.exports = UserController;
