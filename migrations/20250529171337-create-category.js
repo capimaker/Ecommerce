948bcab745a687499acefeb07a050f626b45cb62
capimaker <capimaker@gmail.com>
2025-06-02 14:09:05 +0200

    ultimisimo commit

--- migrations/20250529171337-create-category.js
+++ migrations/20250529171337-create-category.js
@@ -3,35 +3,32 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Categories', [
-      {
-        name_category: 'Deportivo',
-        description: 'Coches de alto rendimiento y velocidad como Ferrari o Lamborghini.',
-        createdAt: new Date(),
-        updatedAt: new Date()
+    await queryInterface.createTable('Categories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
       },
-      {
-        name_category: 'SUV',
-        description: 'Vehículos deportivos utilitarios de lujo como el Range Rover Sport.',
-        createdAt: new Date(),
-        updatedAt: new Date()
+      name_category: {
+        type: Sequelize.STRING,
+        allowNull: false
       },
-      {
-        name_category: 'Eléctrico',
-        description: 'Coches de lujo eléctricos como el Tesla Model S o Porsche Taycan.',
-        createdAt: new Date(),
-        updatedAt: new Date()
+      description: {
+        type: Sequelize.STRING
       },
-      {
-        name_category: 'Clásico',
-        description: 'Modelos antiguos de colección con valor histórico.',
-        createdAt: new Date(),
-        updatedAt: new Date()
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
       }
-    ], {});
+    });
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Categories', null, {});
+    await queryInterface.dropTable('Categories');
   }
 };
